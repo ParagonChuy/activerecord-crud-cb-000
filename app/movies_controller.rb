6eeae60296eb8c5059f7ec34993baedc7b407c48
Jesus Vega <chuyspc@gmail.com>
2017-02-07 00:30:39 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -54,22 +54,22 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movies.where('movies > 2002').sort_by("DESC")
+  Movie.where('release_date > 2002').order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
-  # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
+  # Update the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  m = Movie.find_by(title: "Awesome Flick")
+  m.update(title: "Even Awesomer Flick")
+  m.save
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  m = Movie.find_by(title: "Wat?")
+  m.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
@@ -77,7 +77,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.update_all(title: "A Movie")
 end
 
 def can_destroy_a_single_item
